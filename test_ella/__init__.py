6959f7576c4e4993f060adf2720ffe15380a9222
michal dub <michal.dub@sanomamedia.cz>
2016-02-23 16:38:38 +0100

    fixed tests init for all supported django versions

--- test_ella/__init__.py
+++ test_ella/__init__.py
@@ -13,23 +13,25 @@ old_config = None
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'test_ella.settings'
 
+try:
+    # init django > 1.8
+    django.setup()
+except AttributeError:
+    pass
+
 
 def setup():
     global test_runner
     global old_config
-    from ella.utils.installedapps import call_modules
+
     try:
         from django.test.runner import DiscoverRunner as TestRunner
     except ImportError:
         from django.test.simple import DjangoTestSuiteRunner as TestRunner
-    try:
-        django.setup()
-    except AttributeError:
-        pass
+
     test_runner = TestRunner()
     test_runner.setup_test_environment()
     old_config = test_runner.setup_databases()
-    call_modules(('register', ))
 
 
 def teardown():
--- test_ella/urls.py
+++ test_ella/urls.py
@@ -3,6 +3,10 @@ try:
 except ImportError:
     from django.conf.urls.defaults import patterns, include
 
+from ella.utils.installedapps import call_modules
+
+call_modules(('register', ))
+
 urlpatterns = patterns('',
     (r'^', include('ella.core.urls')),
 )
