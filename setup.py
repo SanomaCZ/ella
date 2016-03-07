aa59a4a73f2436349e2d6d1752493a5bda52f07f
michal dub <michal.dub@sanomamedia.cz>
2016-03-07 16:05:15 +0100

    removed python 2.6 from setup.py and travis

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Framework :: Django",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
