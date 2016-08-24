146306b11000e5b768d21947d93110de25c0ffd4
michal dub <michal.dub@sanomamedia.cz>
2016-08-24 17:14:41 +0200

    used new style of url patterns for test working_urls.py module

--- test_ella/working_urls.py
+++ test_ella/working_urls.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from django.conf import settings
-
-try:
-    from django.conf.urls import *
-except ImportError:
-    from django.conf.urls.defaults import *
+from django.conf.urls.static import static
+from django.conf.urls import url, include
 
 from test_ella.urls import urlpatterns
 
-admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^%s(?P<path>.*)$' % (settings.MEDIA_URL.lstrip('/')), 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT, }),
-    (r'^admin/', include(admin.site.urls)),
-) + urlpatterns
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+] + urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
